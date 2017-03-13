1b7f39977a53c8d2b86334f88b8ea5513e8b0ddb
Andres Ceballos Sanchez <andresrxn10@hotmail.com>
2017-03-12 22:01:11 -0500

    Vista implementada

--- src/app/mean-std/mean-std.component.ts
+++ src/app/mean-std/mean-std.component.ts
@@ -6,10 +6,10 @@ import { Vector } from '../classes/Vector/vector';
   styleUrls: ['./mean-std.component.css']
 })
 export class MeanStdComponent implements OnInit {
-  nombre:string = 'ceballos'; 
+  nombre:string = 'ceballos';
   vector: Vector;
   vectores:Array<Vector> = [];
-  
+
   constructor() { }
 
   ngOnInit() {
@@ -17,18 +17,20 @@ export class MeanStdComponent implements OnInit {
   }
 
   cargarVector(){
-    let vectores = [[1,2,3],[4,5,6]];
+    let vectores = [[1,2,3,343,346,565,2,3434,65,63,12343,423],
+                    [423,232,236,21312,23213,23,213,23,23,232345,78712],
+                    [2354,13,787,43,456,34,7657,234,6575,24,7666,24]];
     for(let i in vectores){
       let vec = new Vector();
       vec.setVector(vectores[i]);
       this.vectores.push(vec);
     }
-    
+    this.vector = this.vectores[0];
   }
 
   seleccionarVector(vector){
     this.vector = vector;
   }
-  
+
 
 }
